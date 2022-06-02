b1e6aa38ed1dc4f9ad65e4af9f7a88910101ee93
Dilan Udawattha <87587934+Dil2010@users.noreply.github.com>
2022-06-02 08:15:05 -0700

    First Commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { HiArrowNarrowRight } from 'react-icons/hi';
+
+const Home = () => {
+  return (
+    <div name='home' className='w-full h-screen bg-[#0a192f]'>
+      {/* Container */}
+      <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>        
+        <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>
+          Dilan Udawattha
+        </h1>
+        <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>
+          I'm a Data Scientist and a Full Stack Developer.
+        </h2>
+        <p className='text-[#8892b0] py-4 max-w-[700px]'>
+          I’m a full-stack developer specializing in building dynamic web and 
+          mobile applications. Presently, I’m focused on
+          building apps using Reactjs and Nextjs.
+        </p>
+        <div>
+          <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>
+            View Work
+            <span className='group-hover:rotate-90 duration-300'>
+              <HiArrowNarrowRight className='ml-3 ' />
+            </span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/components/Work.jsx
@@ -0,0 +1,170 @@
+import React from 'react';
+import Amazing from '../assets/work/Amazing.png';
+import DailyExpenses from '../assets/work/DailyExpenses.png';
+import Poromora_UI from '../assets/work/Poromora_UI.png';
+import Rock from '../assets/work/Rock.png';
+import Calculator from '../assets/work/Calculator.png';
+import Breakout from '../assets/work/Breakout.png';
+
+const Work = () => {
+  return (
+    <div name='work' className='w-full md:h-screen text-gray-300 bg-[#0a192f]'>
+      <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+        <div className='pb-8'>
+          <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600'>
+            Work
+          </p>
+          {/* <p className='py-6'>// Check out some of my recent work</p> */}
+        </div>
+
+{/* Container */}
+        <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
+
+            {/* Grid Item */}
+          <div
+            style={{ backgroundImage: `url(${Amazing})` }}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+          >
+            {/* Hover Effects */}
+            <div className='opacity-0 group-hover:opacity-100'>
+              <span className='text-2xl font-bold text-white tracking-wider'>
+                Java Script Application
+              </span>
+              <div className='pt-8 text-center'>
+                <a href='https://www.youtube.com/watch?v=JZj5PRYd6Cs'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Demo
+                  </button>
+                </a>
+                <a href='https://github.com/Dil2010/Amazing_Travel_Website'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div
+            style={{ backgroundImage: `url(${DailyExpenses})` }}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+          >
+            {/* Hover Effects */}
+            <div className='opacity-0 group-hover:opacity-100'>
+              <span className='text-2xl font-bold text-white tracking-wider'>
+                React JS Application
+              </span>
+              <div className='pt-8 text-center'>
+                <a href='https://www.youtube.com/watch?v=FbrYTjVB8w4'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Demo
+                  </button>
+                </a>
+                <a href='https://github.com/Dil2010/DailyExpenses_App'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+            {/* Grid Item */}
+          <div
+            style={{ backgroundImage: `url(${Poromora_UI})` }}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+          >
+            {/* Hover Effects */}
+            <div className='opacity-0 group-hover:opacity-100'>
+              <span className='text-2xl font-bold text-white tracking-wider'>
+                React JS Application
+              </span>
+              <div className='pt-8 text-center'>
+                <a href='https://www.youtube.com/watch?v=y9E9beATCSY'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Demo
+                  </button>
+                </a>
+                <a href='https://github.com/Dil2010/Pomodoro_Timer_App'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div
+            style={{ backgroundImage: `url(${Rock})` }}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+          >
+            {/* Hover Effects */}
+            <div className='opacity-0 group-hover:opacity-100'>
+              <span className='text-2xl font-bold text-white tracking-wider'>
+                Java Script Application
+              </span>
+              <div className='pt-8 text-center'>
+                <a href='https://www.youtube.com/watch?v=Cdsk5xopJSE'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Demo
+                  </button>
+                </a>
+                <a href='https://github.com/Dil2010/Rock_Paper_Scissors_Game'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+            {/* Grid Item */}
+          <div
+            style={{ backgroundImage: `url(${Calculator})` }}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+          >
+            {/* Hover Effects */}
+            <div className='opacity-0 group-hover:opacity-100'>
+              <span className='text-2xl font-bold text-white tracking-wider'>
+                React JS Application
+              </span>
+              <div className='pt-8 text-center'>
+                <a href='https://www.youtube.com/watch?v=MDpPQEO-b2I'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Demo
+                  </button>
+                </a>
+                <a href='https://github.com/Dil2010/Calculator_App'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div
+            style={{ backgroundImage: `url(${Breakout})` }}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+          >
+            {/* Hover Effects */}
+            <div className='opacity-0 group-hover:opacity-100'>
+              <span className='text-2xl font-bold text-white tracking-wider'>
+                Java Script Application
+              </span>
+              <div className='pt-8 text-center'>
+                <a href='https://github.com/Dil2010/Breakout_Game'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Demo
+                  </button>
+                </a>
+                <a href='https://github.com/Dil2010/Breakout_Game'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Work;
